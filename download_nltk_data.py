5642415b1efcd41b02da00181711848bcdc67c81
sujithy22 <sujithyeruva@gmail.com>
2025-04-10 17:53:20 -0400

    change1

---
+++ download_nltk_data.py
@@ -0,0 +1,10 @@
+import nltk
+
+# Download required NLTK data
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+nltk.download('stopwords')
+
+print("NLTK data downloaded successfully!") 
